4948560d7f0311cb91bce08931e320282bf5b6d5
rysrecn <rysrecn@wwu.edu>
2020-07-22 10:08:25 -0700

    added level 2

---
+++ DynamiPong/Assets/Scripts/Level2/Level2LevelCanvas.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2LevelCanvas : LevelCanvasBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+
+    }
+
+}
